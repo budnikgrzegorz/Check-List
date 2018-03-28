f6508c800b791edc04c56ad94f13a5931d4b2914
Grzegorz_B <budnikgrzegorz@interia.pl>
2018-03-28 22:03:32 +0200

    My first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+
+  newTask: string;
+  tasksList: Array<string> = [];
+  tasksDone: Array<string> = [];
+
+  add() {
+    this.tasksList.push(this.newTask);
+    this.newTask = '';
+  }
+
+  remove(task: string) {
+    this.tasksList = this.tasksList.filter(e => e !== task);
+  }
+
+  done(task: string) {
+    this.tasksDone.push(task);
+    this.remove(task);
+  }
+}
